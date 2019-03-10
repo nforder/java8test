85d64376961093667eb6774b0e6ab5b0b9a54220
nforder <nickforder@icloud.com>
2019-03-10 20:34:41 +0000

    .

--- src/main/java/uk/co/gynesta/Example2.java
+++ src/main/java/uk/co/gynesta/Example2.java
@@ -1,7 +1,22 @@
 package uk.co.gynesta;
 
+import java.util.function.IntFunction;
+
 /**
  * Created by fordern on 10/03/2019.
  */
 public class Example2 {
+
+    public static void main(String[] args) {
+
+        int[] values = {2,3,4};
+
+        IntFunction getCubed = (value) -> Math.round(Math.pow(value, 3));
+
+        for (int value : values) {
+            System.out.println(value + " cubed is " + getCubed.apply(value));
+        }
+
+    }
+
 }
